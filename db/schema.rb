3666afe889f0b5f81a837994986d82884c6c80c2
Stephanie Hutson <stephanie.hutson@appfolio.com>
2020-01-13 11:17:29 -0800

    smh - add images migration and model. Update db pointer
    Adds the basic framework for the images resource.
    Does not include views or controllers as of yet.

--- db/schema.rb
+++ db/schema.rb
@@ -10,6 +10,12 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 0) do
+ActiveRecord::Schema.define(version: 2020_01_08_004615) do
+
+  create_table "images", force: :cascade do |t|
+    t.string "image_url"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
 
 end
