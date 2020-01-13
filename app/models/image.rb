fc30b7ea58a0d3fcd62ed06d2a93fe19d54028c6
Stephanie Hutson <stephanie.hutson@appfolio.com>
2020-01-13 12:11:46 -0800

    smh - add basic validation to url input.

--- app/models/image.rb
+++ app/models/image.rb
@@ -1,3 +1,5 @@
 class Image < ApplicationRecord
-  validates :image_url, presence: true
+  validates :image_url, presence: true, format: {
+    with: %r{/(http|https):\/\/*/}, message: 'Requires valid url (http/https required)'
+  }
 end
