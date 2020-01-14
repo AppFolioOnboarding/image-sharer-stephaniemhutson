fc5c9200464ba8f928a9e66dafa8477d10ee868b
Stephanie Hutson <stephanie.hutson@appfolio.com>
2020-01-14 10:48:23 -0800

    smh - Add seeded images in seed file.
    Source: https://www.canva.com/learn/book-cover-design-50-amazing-covers-you-will-want-to-pick-up/

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,29 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+Image.create([
+  { image_url: 'https://www.canva.com/learn/wp-content/uploads/2015/03/thewealthofthenations-tb-800x0.jpg' },
+  { image_url: 'https://www.canva.com/learn/wp-content/uploads/2015/02/needsimprovement-tb-800x0.jpg' },
+
+  { image_url: 'https://www.canva.com/learn/wp-content/uploads/2015/03/thewealthofthenations-tb-800x0.jpg' },
+  { image_url:
+    'https://www.canva.com/learn/wp-content/uploads/2015/03/gabriel_garcia_marquez.large_-tb-800x0.jpg' },
+  { image_url:
+    'https://www.canva.com/learn/wp-content/uploads/2015/03/0f2faef6c2c98a7bf1c01423ef814f23-tb-800x0.jpg' },
+  { image_url: 'https://www.canva.com/learn/wp-content/uploads/2015/03/tokyoonfoot-tb-800x0.jpg' },
+  { image_url: 'https://www.canva.com/learn/wp-content/uploads/2015/03/redqueen-tb-800x0.jpg' },
+  { image_url: 'https://www.canva.com/learn/wp-content/uploads/2015/03/devilinthedetails-tb-800x0.jpg' },
+  { image_url: 'https://www.canva.com/learn/wp-content/uploads/2015/03/juliovernes-tb-800x0.jpg' },
+  { image_url: 'https://www.canva.com/learn/wp-content/uploads/2015/03/treasureisland-tb-800x0.jpg' },
+  { image_url: 'https://www.canva.com/learn/wp-content/uploads/2015/03/theflamealphabet-tb-800x0.jpg1x' },
+  { image_url: 'https://www.canva.com/learn/wp-content/uploads/2015/03/theboywhocouldnt-tb-800x0.jpg' },
+  { image_url: 'https://www.canva.com/learn/wp-content/uploads/2015/03/insideoutandbackagain-tb-800x0.jpg' },
+  { image_url: 'https://www.canva.com/learn/wp-content/uploads/2015/03/nobodyisevermissing-tb-800x0.jpg' },
+  { image_url: 'https://www.canva.com/learn/wp-content/uploads/2015/02/jaws-tb-800x0.jpg' },
+  { image_url: 'https://www.canva.com/learn/wp-content/uploads/2015/03/middlec-tb-800x0.jpg1x' },
+  { image_url: 'https://www.canva.com/learn/wp-content/uploads/2015/03/deptofspeculation-tb-800x0.jpg' },
+  { image_url: 'https://www.canva.com/learn/wp-content/uploads/2015/03/draculabramstoker-tb-800x0.jpg' },
+  { image_url: 'https://www.canva.com/learn/wp-content/uploads/2015/03/Bad-Teeth-Dustin-Long-tb-800x0.jpg' },
+  { image_url: 'https://www.canva.com/learn/wp-content/uploads/2015/02/endoffood-tb-800x0.jpg' }
+])
